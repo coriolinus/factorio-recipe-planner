9b680dc085ee692eb37a3225d0daf12d931bc58a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-02-10 01:06:31 +0100

    improve recipe tester

--- data-parser/src/bin/test-recipes.rs
+++ data-parser/src/bin/test-recipes.rs
@@ -14,6 +14,10 @@ struct Args {
     /// Print the source value for this recipe.
     #[arg(long)]
     examine_value: Option<String>,
+
+    /// Print the reserialized value for this recipe.
+    #[arg(long)]
+    examine_reserialized: Option<String>,
 }
 
 fn main() -> Result<()> {
@@ -27,11 +31,11 @@ fn main() -> Result<()> {
     let mut recipe_count = 0;
     let mut ok_count = 0;
     let mut parse_value_error = 0;
-    let mut parse_str_error = 0;
-    let mut parse_str_unexpected_ok = 0;
+    let mut different_reserialization = 0;
 
     let mut examined_value = None;
     let mut examined_recipe = None;
+    let mut examined_reserialized = None;
 
     for (name, value) in recipes.into_iter() {
         recipe_count += 1;
@@ -42,7 +46,7 @@ fn main() -> Result<()> {
             }
         }
 
-        let wrapped_recipe = match serde_json::from_value::<Recipe>(value.clone()) {
+        let recipe = match serde_json::from_value::<Recipe>(value.clone()) {
             Ok(recipe) => {
                 ok_count += 1;
                 recipe
@@ -56,25 +60,33 @@ fn main() -> Result<()> {
                     err = dyn_err.source();
                 }
 
-                let compact =
-                    serde_json::to_string(&value).context("generating compact str from value")?;
-                match serde_json::from_str::<Recipe>(&compact) {
-                    Ok(_) => {
-                        parse_str_unexpected_ok += 1;
-                        println!("but parsing from string was ok!")
-                    }
-                    Err(_) => {
-                        parse_str_error += 1;
-                    }
-                };
-
                 continue;
             }
         };
 
+        let Ok(reserialized) = serde_json::to_string(&recipe) else {
+            println!("{name}: reserialization error");
+            continue;
+        };
+        let Ok(deserialized_value) = serde_json::from_str::<Value>(&reserialized) else {
+            println!("{name}: failed to deserialize reserialized value");
+            continue;
+        };
+
+        if value != deserialized_value {
+            different_reserialization += 1;
+            println!("{name}: different `serde_json::Value`; possible missing fields");
+        }
+
         if let Some(examine) = &args.examine {
             if examine == &name {
-                examined_recipe = Some(wrapped_recipe);
+                examined_recipe = Some(recipe);
+            }
+        }
+
+        if let Some(examine) = &args.examine_reserialized {
+            if examine == &name {
+                examined_reserialized = Some(deserialized_value);
             }
         }
     }
@@ -83,14 +95,24 @@ fn main() -> Result<()> {
     println!("validated parsing of {recipe_count} recipes:");
     println!("  {ok_count} parsed ok");
     println!("  {parse_value_error} parse errors");
-    println!("    {parse_str_error} also failed when parsed as a string");
-    println!("    {parse_str_unexpected_ok} succeeded as string parse though");
+    println!("  {different_reserialization} possible missing fields");
 
     if let Some(examine) = &args.examine_value {
         println!();
         match examined_value {
             Some(value) => {
-                println!("{examine}:");
+                println!("{examine} (original value):");
+                println!("{value:#}");
+            }
+            None => println!("{examine} not found in recipes list"),
+        }
+    }
+
+    if let Some(examine) = &args.examine_reserialized {
+        println!();
+        match examined_reserialized {
+            Some(value) => {
+                println!("{examine} (reserialized value):");
                 println!("{value:#}");
             }
             None => println!("{examine} not found in recipes list"),
